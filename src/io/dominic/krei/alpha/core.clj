9925c40b805a982716fe32aef40e632f16f01976
Malcolm Sparks <malcolm@juxt.pro>
2018-08-02 16:17:09 +0200

    Avoid re-parsing krei-files in `build-sass`
    This was an oversight.

--- src/io/dominic/krei/alpha/core.clj
+++ src/io/dominic/krei/alpha/core.clj
@@ -212,30 +212,27 @@
 ;; Sass
 
 (defmethod init! :krei.sass/files [_ files opts]
-  opts)
+  (assoc opts :files (into [] cat files)))
 
 (defn build-sass
-  [target]
+  [files target]
   ;; figwheel can't handle the deleting of this directory, and just blows up,
   ;; so leave stale files hanging around, it'll be fine, he says.
   ;; (fs/delete-dir "./target/public/css/")
   (log/info "Building Sass, output going to %s" target)
-  (let [krei-files (map read-krei-file (find-krei-files))]
-    (run!
-      (fn [[input-file relative-path]]
-        (sass/sass-compile-to-file
-          input-file
-          (io/file target
-                   (string/replace relative-path #"\.scss$" ".css"))
-          ;; TODO: Take options
-          {}))
-      (eduction
-        (map :krei.sass/files)
-        cat
-        (map (juxt (comp io/resource) identity))
-        krei-files))))
+  (run!
+    (fn [[input-file relative-path]]
+      (sass/sass-compile-to-file
+        input-file
+        (io/file target
+                 (string/replace relative-path #"\.scss$" ".css"))
+        ;; TODO: Take options
+        {}))
+    (eduction
+      (map (juxt (comp io/resource) identity))
+      files)))
 
 (defmethod notify! :krei.sass/files [_ events init-result]
   (assert (:krei/target init-result))
   (when (some #(re-matches #".*\.s[ca]ss$" (.getName (:file %))) events)
-    (build-sass (:krei/target init-result))))
+    (build-sass (:files init-result) (:krei/target init-result))))
